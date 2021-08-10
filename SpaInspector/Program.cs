1edf1ef7672840a0bc59d470b561089c86a2ee13
Katerina Komarkova <58290791+kkomarkova@users.noreply.github.com>
2021-08-10 23:54:43 +0200

    Deleting all the changes I pushed directly..

--- SpaInspector/Program.cs
+++ SpaInspector/Program.cs
@@ -15,7 +15,7 @@ namespace SpaInspector
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient {BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)});
-            builder.Services.AddMudServices();
+            
             await builder.Build().RunAsync();
         }
     }
