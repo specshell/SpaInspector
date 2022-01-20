6f7c192e3f7896446e9f69ed0da467a86aeb748e
Michael Pohl <47821325+pohldk@users.noreply.github.com>
2022-01-20 22:53:16 +0100

    adding writeSupport for yUnit fields (#82)
    Co-authored-by: magnaw <magnus_enevoldsen@live.dk>

---
+++ tests/SpaFileReader.Tests/WriterTest.cs
@@ -0,0 +1,33 @@
+﻿namespace SpaFileReader.Tests;
+
+public class WriterTest
+{
+    private readonly byte[] _bytes;
+    private readonly byte[] _noInterferogramBytes;
+
+    public WriterTest()
+    {
+        _bytes = File.ReadAllBytes(TestFile("test1.spa"));
+        _noInterferogramBytes = File.ReadAllBytes(TestFile("test2.spa"));
+    }
+
+    [Fact]
+    public void WriteYUnitAsBytesTest()
+    {
+        var convertedSpa = SpaFile.WriteYUnitAsSpanByte(_bytes, SpaFile.ReadYUnitAsSpanByte(_noInterferogramBytes));
+        var spaToWrite = SpaFile.ReadYUnitAsFloatArray(_noInterferogramBytes);
+        var writtenSpa = SpaFile.ReadYUnitAsFloatArray(convertedSpa.ToArray());
+        Assert.Equal(spaToWrite, writtenSpa);
+    }
+
+    [Fact]
+    public void WriteYUnitAsFloatsTest()
+    {
+        var convertedSpa = SpaFile.WriteYUnitAsSpanFloat( _bytes, SpaFile.ReadYUnitAsFloatArray(_noInterferogramBytes));
+        var spaToWrite = SpaFile.ReadYUnitAsFloatArray(_noInterferogramBytes);
+        var writtenSpa = SpaFile.ReadYUnitAsFloatArray(convertedSpa.ToArray());
+        Assert.Equal(spaToWrite, writtenSpa);
+    }
+
+
+}
